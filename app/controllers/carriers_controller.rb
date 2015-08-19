75ab186fbc96bdf06488448dde744ae34c75c5b3
Carly Jugler <carjug@gmail.com>
2015-08-19 11:04:45 -0700

    Adds stuff to carriers controller for shipping handling

--- app/controllers/carriers_controller.rb
+++ app/controllers/carriers_controller.rb
@@ -19,6 +19,45 @@ class CarriersController < ApplicationController
   end
 
   def fedex_shipping
-      @response = FEDEX.find_rates(params[:origin], params[:destination], params[:packages])
+    origin      = set_origin
+    destination = set_destination
+    packages    = set_packages
+
+    @response = FEDEX.find_rates(origin, destination, packages)
+  end
+
+  def set_origin(city, state, zip, country)
+    ActiveShipping::Location.new(
+      city: city,
+      state: state,
+      zip: zip,
+      country: country
+      )
+  end
+
+  def set_destination(city, state, zip, country)
+    ActiveShipping::Location.new(
+      city: city,
+      state: state,
+      zip: zip,
+      country: country
+      )
+  end
+
+  def set_packages(array)
+    packages = []
+    array.each do |p|
+      package = ActiveShipping::Package.new(
+        # p.weight
+        # p.array of dimensions
+        )
+      packages << package
+    end
+    return packages
   end
 end
+
+# set fedex_shipping endpoint in routes
+# create a new origin
+# create a new destination
+# create new packages with the info from bEtsy
