504ac4e5875a56357b89fb2e48fa4236fd1a0be1
Carly Jugler <carjug@gmail.com>
2015-08-19 17:03:43 -0700

    WIP carriers controller specs

--- spec/controllers/carriers_controller_spec.rb
+++ spec/controllers/carriers_controller_spec.rb
@@ -3,47 +3,62 @@ require 'rails_helper'
 RSpec.describe CarriersController, type: :controller do
 
   describe "FedEx API" do
-    let(:fedex) { ActiveShipping::FedEx.new(
-      login: ENV["FEDEX_LOGIN"],
-      password: ENV["FEDEX_PASSWORD"],
-      meter: ENV["FEDEX_METER"],
-      key: ENV["FEDEX_KEY"],
-      account: ENV["FEDEX_ACCT_NUM"],
-      test: true
-    ) }
+    # let(:fedex) { ActiveShipping::FedEx.new(
+    #   login:    ENV["FEDEX_LOGIN"],
+    #   password: ENV["FEDEX_PASSWORD"],
+    #   meter:    ENV["FEDEX_METER"],
+    #   key:      ENV["FEDEX_KEY"],
+    #   account:  ENV["FEDEX_ACCT_NUM"],
+    #   test: true
+    # ) }
 
-    let(:origin) { ActiveShipping::Location.new(
-      city: "Great Bend",
-      state: "KS",
-      zip: "67530",
-      country: "US"
-    ) }
+    # let(:origin) {
+    #   city: "Great Bend",
+    #   state: "KS",
+    #   zip: "67530",
+    #   country: "US"
+    # }
 
-    let(:destination) { ActiveShipping::Location.new(
-      city: "Beverly Hills",
-      state: "CA",
-      zip: "90210",
-      country: "US"
-    ) }
+    # let(:destination) {
+    #   city: "Beverly Hills",
+    #   state: "CA",
+    #   zip: "90210",
+    #   country: "US"
+    # }
 
-    let(:package) { ActiveShipping::Package.new(
-      100, [25, 40, 30]
-    ) }
-
-    context "response from active_shipping::FedEx" do
-      it "is successful" do
-        fedex
-        expect(response.response_code).to eq(200)
-      end
-
-      # it "fails when invalid credentials (400)" do
-      #   fedex_invalid
-      #   expect(response.response_code).to eq(400)
-      # end
+    # let(:packages) {
+    #   [
+    #     [100, [25, 40, 30]]
+    #     [100, [25, 40, 30]
+    #   ]
+    # }
 
       describe "#index" do
-        it "returns json" do
+        it "is not successful without params" do
           get :index
+
+          expect(response).to not_be_successful
+        end
+
+        it "returns json" do
+          post :index, {"origin"=>{
+            "city"=>"Great Bend",
+            "state"=>"KS",
+            "zip"=>"67530",
+            "country"=>"US"
+            },
+          "destination"=>{
+            "city"=>"SEATTLE",
+            "state"=>"WA",
+            "zip"=>"98112"
+          },
+           "packages"=>[
+            [20, [20, 10, 10]],
+            [20, [20, 10, 10]],
+            [20, [20, 10, 10]]
+          ]
+        }
+          # binding.pry
 
           expect(response.header['Content-Type']).to include 'application/json'
         end
@@ -72,6 +87,6 @@ RSpec.describe CarriersController, type: :controller do
       #     expect(@rate.class).to be_an_instance_of Array
       #   end
       # end
-    end
+    # end
   end
 end
